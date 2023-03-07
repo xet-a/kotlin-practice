9ac608b9e1f92996acb7963be79355848fc2a4b9
xet-a <csh4519@naver.com>
2023-03-07 16:56:29 +0900

    Refactor: add dialog를 bottom sheet dialog fragment로 변경, fragment 구조로 변경

---
+++ ToDoList/app/src/main/java/com/example/todolist/view/fragment/AddFragment.kt
@@ -0,0 +1,56 @@
+package com.example.todolist.view.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import com.example.todolist.databinding.FragmentAddBinding
+import com.example.todolist.model.ToDoModel
+import com.example.todolist.viewmodel.ToDoViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.util.*
+
+class AddFragment(context: Context): BottomSheetDialogFragment() {
+    private val mContext: Context = context
+    private val toDoViewModel by viewModels<ToDoViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = FragmentAddBinding.inflate(inflater)
+        val btnOk: Button = view.btnOk
+        val btnClose: Button = view.btnClose
+
+        val addTitle: EditText by lazy {
+            view.addTitle
+        }
+        val addContent: EditText by lazy {
+            view.addContent
+        }
+
+        btnOk.setOnClickListener {
+            var id: Long? = null
+            val title = addTitle.text.toString()
+            val content = addContent.text.toString()
+            val createdDate = Date().time
+            val toDoModel = ToDoModel(id, title, content, createdDate)
+            toDoViewModel.insert(toDoModel)
+
+            Toast.makeText(mContext, "등록되었습니다.", Toast.LENGTH_SHORT).show()
+            dismiss()
+            // fragment에 남아있는 값 reset하기
+        }
+        btnClose.setOnClickListener {
+            Toast.makeText(mContext, "취소되었습니다.", Toast.LENGTH_SHORT).show()
+            dismiss()
+        }
+
+        return view.root
+    }
+}
